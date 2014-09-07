caf14340770e9d79ecab76e84cca47d22f9e6adc
Daniel Sarfati <daniel@zipdigs.com>
2014-09-07 01:02:22 -0700

    Changed version to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='usshapes',
-      version='1.0',
+      version='0.1',
       description='U.S. Shapefile Indexer for Elasticsearch',
       author='Daniel Sarfati',
       author_email='daniel@knockrentals.com',
